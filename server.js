21147469e723b4f3d8b24fd513fc4c852bee9fbc
Alex Lee <doomcat9167@gmail.com>
2017-07-20 19:53:32 -0700

    reset twilio commits

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var port = process.env.PORT || 3000;
+var sms = require('./public/js/sms.js');
 var express = require('express');
 
 var app = express();
@@ -7,5 +8,13 @@ app.get('/', function(req, res) {
   res.sendfile('./public/index.html');
 });
 
+app.post('/message', function(req, res) {
+  // var message = req.body.message;
+  // var toPhoneNumber = req.body.toPhoneNumber;
+  sms.sendSms('5039983176', 'message');
+  // console.log(message);
+
+  res.sendfile('./public/index.html');
+});
 
 app.listen(port);
