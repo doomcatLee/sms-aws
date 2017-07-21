f206cf28a50b609a31a6cdc17d42edd4ca4a5290
Alex Lee <doomcat9167@gmail.com>
2017-07-20 20:03:29 -0700

    fix the error

--- server.js
+++ server.js
@@ -2,17 +2,32 @@ var port = process.env.PORT || 3000;
 var sms = require('./public/js/sms.js');
 var express = require('express');
 
+var http = require('http');
+
+var bodyParser = require('body-parser');
+var request = require('request');
+var router = express.Router();
+
 var app = express();
+
+
+//THIS IS SO YOU CAN TAKE HTML FORM DATA INTO APP.JS
+app.use(express.static('public'));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // support encoded bodies
+
 
 app.get('/', function(req, res) {
   res.sendfile('./public/index.html');
 });
 
 app.post('/message', function(req, res) {
-  // var message = req.body.message;
-  // var toPhoneNumber = req.body.toPhoneNumber;
-  sms.sendSms('5039983176', 'message');
-  // console.log(message);
+  var message = req.body.message;
+  var toPhoneNumber = req.body.toPhoneNumber;
+  sms.sendSms(toPhoneNumber,message);
+  console.log(message);
 
   res.sendfile('./public/index.html');
 });
